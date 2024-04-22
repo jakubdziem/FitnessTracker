03b5a46a531845420bbc3b56d2c7ae1bb30a0835
Jakub Dziem <jakub.dziem9@gmail.com>
2024-04-22 07:12:58 +0200

    Attempt to solve heroku problem.

--- src/main/java/com/dziem/fitnesstrackerweb/FitnessTrackerWebApplication.java
+++ src/main/java/com/dziem/fitnesstrackerweb/FitnessTrackerWebApplication.java
@@ -2,9 +2,6 @@ package com.dziem.fitnesstrackerweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FitnessTrackerWebApplication {
